6ffdef16740e8fc9859bd16137e09a5c965f3198
alexisdeudon01 <alexisdeudon01@gmail.com>
2026-02-22 05:01:40 +0100

    cxcx

--- install_stack.sh
+++ install_stack.sh
@@ -3,15 +3,14 @@
 
 # 1. Dockerfile (Correctif URL Git + Bypass compilation PyQt5)
 cat <<DOCKER > Dockerfile
-FROM python:3.11-bookworm
+FROM debian:bookworm
 
 ENV DEBIAN_FRONTEND=noninteractive
 ENV QT_QPA_PLATFORM=offscreen
-ENV PYTHONPATH=/usr/lib/python3/dist-packages
 
-# Installation des dépendances via APT avec les bons noms (python3-openssl)
+# Installation des dépendances via APT (Python système unique, pas de conflit)
 RUN apt-get update && apt-get install -y \\
-    build-essential \\
+    build-essential python3 python3-pip python3-setuptools python3-dev \\
     git hostapd dnsmasq iptables wireless-tools net-tools rfkill \\
     python3-pyqt5 python3-sip python3-openssl python3-scapy \\
     python3-netifaces python3-requests python3-psutil \\
@@ -22,7 +21,7 @@ WORKDIR /opt
 RUN git clone https://github.com/P0cL4bs/wifipumpkin3.git /opt/wifipumpkin3
 
 WORKDIR /opt/wifipumpkin3
-# Installation sans re-télécharger PyQt5 (déjà installé via apt)
+# Installation avec le Python système (pas de conflit setuptools)
 RUN pip3 install . --break-system-packages --no-deps
 
 WORKDIR /app
